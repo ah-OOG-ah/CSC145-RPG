0d0483f07cda421cdf0191572a55a084ac1c44dc
MewtwoRBGY <jracca001@student.franciscan.edu>
2024-01-26 12:44:47 -0500

    Made changes to entity.cpp and entity.h such as new stats

--- Entity.cpp
+++ Entity.cpp
@@ -10,3 +10,17 @@ Entity::Entity(int64_t hp, int64_t x, int64_t y) {
     this->x = x;
     this->y = y;
 }
+
+int64_t Entity::getHp() { return hp; }
+
+int64_t Entity::getX() { return x; }
+
+int64_t Entity::getY() { return y; }
+
+int64_t Entity::getAttk() { return attk; }
+
+int64_t Entity::getStaticDef() { return staticDef; }
+
+int64_t Entity::getPercDef() { return percDef; }
+
+int64_t Entity::getSpd() { return spd; }
--- Entity.h
+++ Entity.h
@@ -6,8 +6,17 @@ class Entity {
 
   protected:
     int64_t hp;
-    int64_t x;
+    int64_t x; 
     int64_t y;
+    int64_t attk;
+    int64_t staticDef; //Substracts a set amount of damage from a hit
+    int64_t percDef; //Reduces a specified percentage of damage from an attack after staticDef removes its amount
+    int64_t spd;
+    
+    //Joseph: I want to include an built-in status variable for status effects but should it be a string or an enum?
+
+    /*Joseph: Are these x and y variavles for position? I think Tyler just used the position as an example, 
+    as position does not make sense in turn-based combat*/
 
   public:
     Entity(int64_t hp);
@@ -16,4 +25,20 @@ class Entity {
     int64_t getHp();
     int64_t getX();
     int64_t getY();
+    int64_t getAttk();
+    int64_t getStaticDef();
+    int64_t getPercDef();
+    int64_t getSpd();
+};
+
+class Player : public Entity
+{
+    Player(int64_t hp) : Entity(hp);
+    Player(int64_t hp, int64_t x, int64_t y) : Entity(hp, x, y);
+};
+
+class Enemy : public Entity
+{
+    Enemy(int64_t hp) : Entity(hp);
+    Enemy(int64_t hp, int64_t x, int64_t y) : Entity(hp, x, y);
 };
