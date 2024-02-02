4546390ab3c7a5c38081d165b3103d6fac1bb5ff
MewtwoRBGY <jracca001@student.franciscan.edu>
2024-02-01 20:30:40 -0500

    Added the Item class

---
+++ item.cpp
@@ -0,0 +1,22 @@
+#include "item.h"
+
+#include <cstdint>
+#include <string>
+
+    Item::Item(std::string name, int64_t amnt, int64_t amountDetermine)
+    {
+        itemName = name;
+        amount = amnt;
+        amount = amountDetermine;
+    }
+
+    std::string Item::GetName() { return itemName; }
+    int64_t Item::GetAmount() { return amount; }
+    Item* Item::GetNext() { return nextItem; }
+    int64_t Item::GetAmntTitle() { return amountTitle;}
+
+    void Item::ChangeAmount(int64_t addAmnt) { amount += addAmnt; }
+    void Item::SetNext(Item* next) { nextItem = next; }
+
+    void Item::SetName(std::string newName) { itemName = newName; }
+    void Item::SetAmountTitle(int64_t newTitle) { amountTitle = newTitle; }
---
+++ item.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+class Item
+{
+    protected:
+    std::string itemName;
+    int64_t amount;
+    Item* nextItem;
+    int64_t amountTitle; //Will determine if item say amount, uses, or nothing when going through inven using a switch statement
+    //Was going to use an enum but not sure what to do with GetAmntTitle()
+
+    public:
+    //Constructor
+    Item(std::string name, int64_t amnt, int64_t amountDetermine);
+
+    //Getters
+    std::string GetName();
+    int64_t GetAmount();
+    Item* GetNext();
+    int64_t GetAmntTitle();
+    //Setters
+    void ChangeAmount(int64_t addAmnt);
+    void SetNext(Item* next);
+
+    //Setters I do not think we need
+    void SetName(std::string newName);
+    void SetAmountTitle(int64_t newTitle);
+};
