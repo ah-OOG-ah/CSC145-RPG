0b6b9b59e815d5fbc532ae577d5a9a95f6e7b7b2
Joseph Racca <jracca001@student.franciscan.edu>
2024-03-04 14:19:09 -0500

    Test File for Inventory

--- game.cpp
+++ game.cpp
@@ -4,6 +4,11 @@
 #include "Player.h"
 #include <memory>
 #include "game.h"
+#include "inventory.h"
+#include "item.h"
+#include "RegularItems.h"
+#include <cstdint>
+#include <string>
 
 
 DebugMenu debugMenu = DebugMenu();
@@ -14,6 +19,20 @@ std::shared_ptr<Player> getPlayer() {
 }
 
 int main() {
-
-    debugMenu.display();
+    AttackItem* rock = new AttackItem("Rock", 4, 10);
+    HealItem* potion = new HealItem("Potion", 10, 6);
+    AttackItem* spear = new AttackItem("Spear", 1, 20);
+    AttackItem* sword = new AttackItem("Sword", 2, 15);
+    HealItem* water = new HealItem("Sword", 10, 20);
+    Inventory myInven(5, rock);
+    myInven.PrintInven();
+    myInven.SetElement(1, potion);
+    myInven.PrintInven();
+    myInven.SetElement(1, spear);
+    myInven.SetElement(2, sword);
+    myInven.PrintInven();
+    myInven.SetElement(4, water);
+    myInven.PrintInven();
+    //debugMenu.display();
+    return 0;
 }
