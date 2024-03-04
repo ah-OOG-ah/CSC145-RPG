b64fb524601332a8e7413af8707c038515a2c855
Joseph Racca <jracca001@student.franciscan.edu>
2024-03-04 14:32:36 -0500

    Removed test file

--- game.cpp
+++ game.cpp
@@ -5,15 +5,6 @@
 #include <memory>
 #include "game.h"
 
-
-#include "inventory.h"
-#include "item.h"
-#include "RegularItems.h"
-#include <cstdint>
-#include <string>
-#include <iostream>
-
-
 DebugMenu debugMenu = DebugMenu();
 Player player = Player(10);
 
@@ -22,19 +13,5 @@ std::shared_ptr<Player> getPlayer() {
 }
 
 int main() {
-    AttackItem* rock = new AttackItem("Rock", 4, 10);
-    HealItem* potion = new HealItem("Potion", 10, 6);
-    AttackItem* spear = new AttackItem("Spear", 1, 20);
-    AttackItem* sword = new AttackItem("Sword", 2, 15);
-    HealItem* water = new HealItem("Water", 10, 20);
-    Inventory inven(rock);
-    std::cout<<inven.ToString()<<std::endl;
-    inven.SetElement(1, potion);
-    inven.SetElement(2, spear);
-    std::cout<<inven.ToString()<<std::endl;
-    inven.SetElement(2, nullptr);
-    inven.SetElement(2, sword);
-    inven.SetElement(3, water);
-    std::cout<<inven.ToString()<<std::endl;
-   //debugMenu.display();
+    debugMenu.display();
 }
