b99eb1993ef71cc11c7c5552ca57b0d409efd09e
Joseph Racca <jracca001@student.franciscan.edu>
2024-04-09 15:14:22 -0400

    Added Armor

--- Equipment.cpp
+++ Equipment.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include "Equipment.h"
 
-Equipment(std::string itemName, int64_t durab, int64_t price) : 
+Equipment(std::string itemName, int64_t durab, int64_t price) : Item(itemName, price, durab) {}
 
 std::string Equipment::GetAmntText()
 {
@@ -21,3 +21,13 @@ Weapon::Weapon(std::string itemName, int64_t amnt, int64_t dmg, int64_t price) :
 int64_t Weapon::GetDamage() { return dmgMultiplier; };
 
 void Weapon::SetDamage(int64_t dmg) { dmgMultiplier = dmg; };
+
+Armor::Armor(std::string itemName, int64_t durab, int64_t pDef, int64_t sDef, int64_t price) : Equipment(itemName, durab, price)
+{
+    percDef = pDef;
+    staticDef = sDef;
+}
+int64_t Armor::GetPercDef() { return percDef; }
+int64_t GetStaticDef() { return staticDef; }
+void SetPercDef(int64_t def) { percDef = def; }
+void SetStaticDef(int64_t def) { staticDef = def; }
--- Equipment.h
+++ Equipment.h
@@ -22,3 +22,18 @@ class Weapon : public Equipment
     int64_t GetDamage();
     void SetDamage(int64_t dmg);
 };
+
+class Armor : public Equipment
+{
+    protected:
+    //Defense stats work like those with the player
+    int64_t percDef; 
+    int64_t staticDef; 
+
+    public:
+    Armor(std::string itemName, int64_t durab, int64_t pDef, int64_t sDef, int64_t price);
+    int64_t GetPercDef();
+    int64_t GetStaticDef();
+    void SetPercDef(int64_t);
+    void SetStaticDef(int64_t);
+};
