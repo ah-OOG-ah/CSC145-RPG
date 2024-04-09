3a0d631e20bb44eb30eb2f1f4845a29e3f8026c4
Joseph Racca <jracca001@student.franciscan.edu>
2024-04-09 15:07:54 -0400

    Added Weapons

--- itemlist.cpp
+++ itemlist.cpp
@@ -2,9 +2,18 @@
 #include "RegularItems.h"
 #include "Equipment.h"
 
-AttackItem rock("Rock", 5);
-AttackItem dart("Dart", 8);
+AttackItem rock("Rock", 5, 2);
+AttackItem dart("Dart", 8, 5);
 
-HealItem pureWater("Pure Water", 15);
-HealItem elixir("Elixir", 20);
-HealItem cookie("Cookie", 10);
+HealItem pureWater("Pure Water", 15, 9);
+HealItem elixir("Elixir", 40, 25);
+HealItem cookie("Cookie", 5, 3);
+
+Weapon sword("Sword", 20, 25, 20);
+Weapon spear("Spear", 10, 20, 15);
+Weapon hammer("Gorilla Hammer", 30, 50, 45);
+Weapon staff("Staff", 10, 10, 8);
+Weapon bottle("Broken Bottle", 3, 70, 4);
+Weapon goldSword("Gold Sword", 5, 50, 100);
+Weapon whip("Orphan Whip", 30, 5, 20);
+
