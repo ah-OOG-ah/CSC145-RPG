c5352c2b8ab6bde3df1beca727532eb6e86685e4
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-04-14 00:33:31 -0400

    Add random source

--- game.cpp
+++ game.cpp
@@ -3,11 +3,25 @@
 #include "Menu.h"
 #include "Player.h"
 #include <memory>
+#include <random>
 #include "game.h"
 
 DebugMenu debugMenu = DebugMenu();
 Player player = Player(10);
 std::shared_ptr<Scene> scene;
+
+typedef std::mt19937 RNG;
+RNG rng; // NOLINT(*-msc51-cpp)
+std::random_device sysrng;
+std::uniform_int_distribution<int64_t> all_dist;
+
+void initRng() {
+    rng.seed(all_dist(sysrng));
+}
+
+int64_t getRand() {
+    return all_dist(rng);
+}
 
 std::shared_ptr<Scene> getScene() {
     return scene;
@@ -18,5 +32,7 @@ std::shared_ptr<Player> getPlayer() {
 }
 
 int main() {
+    initRng();
+
     debugMenu.display();
 }
