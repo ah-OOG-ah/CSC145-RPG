7c7832f9ebfa42356839ed78583d9c5a12e0b117
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-04-13 23:06:05 -0400

    Patch Battle and Entity

--- Entity.h
+++ Entity.h
@@ -8,28 +8,28 @@ class Entity {
   protected:
     int64_t hp;
     int64_t attk = 10;
-    int64_t staticDef = 0; //Substracts a set amount of damage from a hit
-    int64_t percDef = 0; //Reduces a specified percentage of damage from an attack after staticDef removes its amount
-    int64_t spd = 10; //Used to determine turn order
-    int64_t mp; //Used to have a magic points sytem if we want include that
+    int64_t staticDef = 0; // Subtracts a set amount of damage from a hit
+    int64_t percDef = 0; // Reduces a specified percentage of damage from an attack after staticDef removes its amount
+    int64_t spd = 10; // Used to determine turn order
+    int64_t mp = 0; // Used to have a magic points system if we want include that
     bool isFleeing = false;
 
   public:
-    Entity(int64_t hp);
+    explicit Entity(int64_t hp);
 
-    int64_t getHp();
-    int64_t getAttk();
-    int64_t getStaticDef();
-    int64_t getPercDef();
-    int64_t getSpd();
-    int64_t getMp();
-    bool getFleeing();
-    bool getAlive();
+    [[nodiscard]] int64_t getHp() const;
+    [[nodiscard]] int64_t getAttk() const;
+    [[nodiscard]] int64_t getStaticDef() const;
+    [[nodiscard]] int64_t getPercDef() const;
+    [[nodiscard]] int64_t getSpd() const;
+    [[nodiscard]] int64_t getMp() const;
+    [[nodiscard]] bool getFleeing() const;
+    [[nodiscard]] bool getAlive() const;
 
     void setFleeing(bool);
 
     // Applies this entitiy's attack to the passed entity
-    void attackEntity(Entity* enemy);
+    void attackEntity(Entity* enemy) const;
 
-    std::string toString();
+    [[nodiscard]] std::string toString() const;
 };
