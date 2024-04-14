fa46ff319c65d078e707d53e7e39ca8e5133a804
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-04-14 02:49:34 -0400

    Barebones run loops

--- Room.cpp
+++ Room.cpp
@@ -6,3 +6,5 @@ std::string Room::GetName() const { return roomName; }
 std::string Room::GetDesc() const { return description; }
 
 std::string Room::GetMapDisplay() const { return isExplored ? mapDisplay : "\?"; }
+
+Room::~Room() = default;
--- scene/Tower.cpp
+++ scene/Tower.cpp
@@ -1,6 +1,7 @@
 #include "Tower.h"
 
 #include <utility>
+#include <iostream>
 
 
 Tower::Tower(std::string name) : Scene(std::move(name)) { }
@@ -9,6 +10,9 @@ void Tower::run() {
 
     // Run and enter the floor
     for (int64_t level = 1; level <= 100; ++level) {
-        floors.emplace_back(level);
+        floors.emplace_back("Floor " + std::to_string(level), level);
+
+        std::cout << "You enter Floor " << level << std::endl;
+        floors[level - 1].run();
     }
 }
