eb20b1b384575ee9bc7c72de9684fe7b9e092e88
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-04-13 23:02:27 -0400

    Fix RegularItems
    It builds, but there's more to do

--- RegularItems.cpp
+++ RegularItems.cpp
@@ -2,26 +2,23 @@
 #include <utility>
 #include "RegularItems.h"
 
-RegularItem(std::string itemName, int64_t price, int64_t amnt) : Item(itemName, price, amnt) {}
-RegularItem(std::string itemName, int64_t price) : Item(itemName, price) {}
+RegularItem::RegularItem(std::string itemName, int64_t price) : RegularItem(std::move(itemName), price, 1) {}
+RegularItem::RegularItem(std::string itemName, int64_t price, int64_t amnt)
+    : Item(std::move(itemName), price, amnt, "FIXME") {}
 
-std::string RegularItem::GetAmntText()
-{
+std::string RegularItem::GetAmntText() {
     return "x";
 }
 
-AttackItem(std::string itemName, int64_t dmg, int64_t price, Status* effect, int64_t chance) : RegularItem(itemName, price) 
-{
+AttackItem::AttackItem(std::string itemName, int64_t dmg, int64_t price, Status* effect, int64_t chance) : RegularItem(std::move(itemName), price) {
     damage = dmg;
     status = effect;
     effectChance = chance;
 }
-AttackItem(std::string itemName, int64_t dmg, int64_t price, int64_t amnt) : RegularItem(itemName, price, amnt)
-{
+AttackItem::AttackItem(std::string itemName, int64_t dmg, int64_t price, int64_t amnt) : RegularItem(std::move(itemName), price, amnt) {
     damage = dmg;
 }
-AttackItem(std::string itemName, int64_t dmg, int64_t price) : RegularItem(itemName, price)
-{
+AttackItem::AttackItem(std::string itemName, int64_t dmg, int64_t price) : RegularItem(std::move(itemName), price) {
     damage = dmg;
 }
 
@@ -29,35 +26,31 @@ void AttackItem::SetDamage(int64_t dmg) { damage = dmg; }
 void AttackItem::SetStatus(Status* effect) { status = effect; }
 void AttackItem::SetChance(int64_t chance) { effectChance = chance; }
 
-int64_t AttackItem::GetDamage() { return damage; }
+int64_t AttackItem::GetDamage() const { return damage; }
 Status* AttackItem::GetStatus() { return status; }
-int64_t AttackItem::GetChance() { return effectChance; }
+int64_t AttackItem::GetChance() const { return effectChance; }
 
-HealItem(std::string itemName, int64_t hp, int64_t price) : RegularItem(itemName, price)
-{
+HealItem::HealItem(std::string itemName, int64_t hp, int64_t price) : RegularItem(std::move(itemName), price) {
     hpAmnt = hp;
 }
-HealItem(std::string itemName, int64_t hp, int64_t price, int64_t amnt) : RegularItem(itemName, price, amnt)
-{
+HealItem::HealItem(std::string itemName, int64_t hp, int64_t price, int64_t amnt) : RegularItem(std::move(itemName), price, amnt) {
     hpAmnt = hp;
 }
 
 void HealItem::SetHpAmnt(int64_t hp) { hpAmnt = hp; }
 void HealItem::SetHealedStatus(Status* status) { healedStatus = status; }
 
-int64_t HealItem::GetHpAmnt() { return hpAmnt; }
+int64_t HealItem::GetHpAmnt() const { return hpAmnt; }
 Status* HealItem::GetHealedStatus() { return healedStatus; }
 
-NonConsumAttackItem::NonConsumAttackItem(std::string itemName, int64_t dmg, int64_t price, Status* effect, int64_t chance) : AttackItem(itemName, dmg, price, effect, chance) 
-{ stackable = false; }
+NonConsumAttackItem::NonConsumAttackItem(std::string itemName, int64_t dmg, int64_t price, Status* effect, int64_t chance)
+    : AttackItem(std::move(itemName), dmg, price, effect, chance) {
+    stackable = false;
+}
 
-NonConsumAttackItem::NonConsumAttackItem(std::string itemName, int64_t dmg, int64_t price) : AttackItem(itemName, dmg, price) 
-{ stackable = false; }
-
-void NonConsumAttackItem::ChangeAmount(int64_t addAmnt) 
-{ 
-    if(addAmnt >= 1) { return; }; 
-    else { amnt = 0; }
+NonConsumAttackItem::NonConsumAttackItem(std::string itemName, int64_t dmg, int64_t price)
+    : AttackItem(std::move(itemName), dmg, price) {
+    stackable = false;
 }
 
 std::string NonConsumAttackItem::GetAmntText() { return " "; }
