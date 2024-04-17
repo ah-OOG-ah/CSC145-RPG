6a7ae2755db273b69c16d980fbce39ecbb7e55cb
Joseph Racca <jracca001@student.franciscan.edu>
2024-04-17 12:21:37 -0400

    Started to revamp Enemy class

--- Enemy.h
+++ Enemy.h
@@ -3,7 +3,9 @@
 #include "Entity.h"
 
 class Enemy : public Entity {
-
+  protected:
+  std::string enemySprite;
   public:
-    Enemy(int64_t hp) : Entity(hp) {}
+    Enemy(std::string name, int64_t hp, int64_t attk, int64_t percDef, int64_t staticDef, int64_t spd);
+    Enemy(std::string name, int64_t hp, int64_t attk, int64_t percDef, int64_t staticDef, int64_t spd, std::string sprite);
 };
