dbe291fdf6cb9be8a2cfa262c1a1f504718e6f11
Joseph Racca <jracca001@student.franciscan.edu>
2024-04-17 13:55:53 -0400

    Started to add Enemy types

--- itemtiers.h
+++ itemtiers.h
@@ -3,9 +3,12 @@
 #include "itemlist.h"
 #include <vector>
 
+std::vector<RegularItem*> noItems = { nullptr };
 std::vector<RegularItem*> basicItems = {&rock, &dart, &pureWater, &cookie, &elixir };
+std::vector<Weapon*> noWeapons = { nullptr };
 std::vector<Weapon*> basicWeapons = {&sword, &spear, &whip, &staff};
 std::vector<Weapon*> rareWeapons = {&sword, &hammer, &goldSword, &bottle};
+std::vector<Armor*> noArmor = { nullptr };
 std::vector<Armor*> basicArmor = {&steelHelm, &steelLegg, &chainChest, &chainBoots, &chainLegg, &chainHelm, &rockChest, &toughVest};
 std::vector<Armor*> betterArmor = {&steelHelm, &steelChest, &steelLegg, &steelBoots, &goldChest, &goldBoots, &goldLegg, &goldHelm, &rockHelm, &rockChest, &rockLegg, &rockBoots, &toughVest, &armyVest};
 std::vector<Armor*> topTierArmor = {&steelHelm, &armyVest, &toughVest, &steelChest, &baboonArmor, &steelLegg, &steelBoots};
