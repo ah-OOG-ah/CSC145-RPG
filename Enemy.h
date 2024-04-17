bf8eed138c4a66c488aadc08a426330ae16aeb07
Joseph Racca <jracca001@student.franciscan.edu>
2024-04-17 13:03:25 -0400

    Added item vectors for enemy generation

--- Enemy.h
+++ Enemy.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "Entity.h"
+#include <vector>
+#include "itemtiers.h"
 
 class Enemy : public Entity {
   protected:
   std::string enemySprite;
   public:
-    Enemy(std::string name, int64_t hp, int64_t attk, int64_t percDef, int64_t staticDef, int64_t spd);
-    Enemy(std::string name, int64_t hp, int64_t attk, int64_t percDef, int64_t staticDef, int64_t spd, std::string sprite);
+    Enemy(std::string name, int64_t hp, int64_t attk, int64_t percDef, int64_t staticDef, int64_t spd, std::vector<RegularItem*> invenSlots, std::vector<Weapon*> weaponSlots, std::vector<Armor*> armorSlots);
+    Enemy(std::string name, int64_t hp, int64_t attk, int64_t percDef, int64_t staticDef, int64_t spd, std::vector<RegularItem*> invenSlots, std::vector<Weapon*> weaponSlots, std::vector<Armor*> armorSlots, std::string sprite);
 };
