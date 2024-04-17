385d71b782a20fcad51a4f5c8489634da3d529ef
Joseph Racca <jracca001@student.franciscan.edu>
2024-04-17 13:55:53 -0400

    Started to add Enemy types

--- enemylist.h
+++ enemylist.h
@@ -3,3 +3,12 @@
 #include "Enemy.h"
 #include "itemtiers.h"
 
+std::string spiderSprite = 
+"     \n"  
+"     \n"
+"/\\O/\\\n"
+"     \n"
+"     \n";
+
+//Enemy(name,hp,attk,percDef,staticDef,spd, invenSlots, weaponSlots, armorSlots, sprite)
+Enemy wideSpider ("WIDE Spider", 30, 10, 10, 5, noItems, noWeapons, noArmor, spiderSprite);
