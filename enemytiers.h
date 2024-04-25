75f8c9890d99e65a7ba2feb24936f4c76de32557
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-04-24 21:17:09 -0400

    Update enemytiers.h

--- enemytiers.h
+++ enemytiers.h
@@ -8,13 +8,16 @@ namespace ETiers {
 
     using namespace EList;
 
+    // Mixed enemy lists
     static const std::vector<Enemy*> basicEnemies = {&wideSpider, &wideSpider, &baboon, &basedBat, &baboon, &angryBaboon};
+    static const std::vector<Enemy*> harderEnemies = {&veryBasedBat, &tradBasedBat, &sentry, &sentry, &steelSentry, &angryBaboon, &zombie, &ghoul, &cryptogriph};
+    static const std::vector<Enemy*> hardEnemies = {&steelSentry, &cryptogriph, &mummy, &zombie};
+
+    // Special sets
     static const std::vector<Enemy*> sentriesOnly = {&smallSentry, &smallSentry, &sentry, &sentry, &sentry, &steelSentry};
     static const std::vector<Enemy*> monkeysOnly = {&baboon, &baboon, &angryBaboon, &angryBaboon, &monkeyGhoul};
     static const std::vector<Enemy*> undeadOnly = {&zombie, &mummy, &ghoul, &monkeyGhoul, &zombie, &mummy, &ghoul, &monkeyGhoul, &cryptogriph};
     static const std::vector<Enemy*> batsOnly = {&basedBat, &basedBat, &basedBat, &veryBasedBat, &veryBasedBat, &tradBasedBat};
-    static const std::vector<Enemy*> harderEnemies = {&veryBasedBat, &tradBasedBat, &sentry, &sentry, &steelSentry, &angryBaboon, &zombie, &ghoul, &cryptogriph};
-    static const std::vector<Enemy*> hardEnemies = {&steelSentry, &cryptogriph, &mummy, &zombie};
 
     std::vector<Enemy*> get(int64_t i) {
         switch (std::min(std::max(0l, i - 1), 6l)) {
