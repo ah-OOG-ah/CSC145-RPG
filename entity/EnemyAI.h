6a6bcf5c6f3962a95f5c318e2a4eb7c6a3e5d1a9
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-04-28 15:44:52 -0400

    Fix amateur AI

--- entity/EnemyAI.h
+++ entity/EnemyAI.h
@@ -9,7 +9,7 @@ namespace EAI {
     static void idiot(const std::shared_ptr<Enemy>& user, const std::vector<std::shared_ptr<Enemy>>& allies);
     static void berserker(const std::shared_ptr<Enemy>& user, const std::vector<std::shared_ptr<Enemy>>& allies);
     static void amateur(const std::shared_ptr<Enemy>& user, const std::vector<std::shared_ptr<Enemy>>& allies);
-    static void expert(std::shared_ptr<Enemy> user, std::vector<std::shared_ptr<Enemy>> allies);
+    static void expert(const std::shared_ptr<Enemy>& user, const std::vector<std::shared_ptr<Enemy>>& allies);
     static void healer(std::shared_ptr<Enemy> user, std::vector<std::shared_ptr<Enemy>> allies);
     static void itemHappy(std::shared_ptr<Enemy> user, std::vector<std::shared_ptr<Enemy>> allies);
 
