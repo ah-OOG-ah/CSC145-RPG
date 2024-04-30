fed47064c678b3ae7ba1b98542f26870ff1852e5
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-04-30 17:18:04 -0400

    Remove unneeded fields

--- scene/rooms/ShopRoom.h
+++ scene/rooms/ShopRoom.h
@@ -12,36 +12,9 @@
 class ShopRoom : public Room {
   protected:
     std::string mapDisplay = "S";
-    std::string merchantName;
-    std::shared_ptr<Item> purchase1;
-    std::shared_ptr<Item> purchase2;
-    std::shared_ptr<Item> purchase3;
 
-    Dialogue dialogue;
     std::unique_ptr<ShopMenu> shopMenu;
 
   public:
     ShopRoom(std::shared_ptr<Movement> m, uint8_t mask, int64_t nice);
-
-    /*
-    Name: GetMerchant
-    Return type: string
-    Arguments: None
-    Use: returns the "merchant" string
-    */
-    [[nodiscard]] std::string GetMerchant() const;
-    /*
-    Name: GetPurchase
-    Return type: Item pointer
-    Arguments: An int64_t integer from 1-3
-    Use: Takes in an integer and returns one of the "purchase" Item pointers based on the integer
-    */
-    std::shared_ptr<Item> GetPurchase(int64_t);
-    /*
-    Name: GetPurchase
-    Return type: string
-    Arguments: An int64_t integer from 1-8
-    Use: Takes in an integer and gives the corresponding dialogue members based on the number given
-    */
-    std::string GetDialogue(int64_t);
 };
