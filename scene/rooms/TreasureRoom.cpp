da1002e04cd2de73100b8594f5217b2d8757fea0
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-05-01 00:07:33 -0400

    Fix weapon switching

--- scene/rooms/TreasureRoom.cpp
+++ scene/rooms/TreasureRoom.cpp
@@ -8,16 +8,16 @@ Item* TreasureRoom::GetTreasure2() { return treasure2; }
 TreasureRoom::TreasureRoom(std::shared_ptr<Movement> m, uint8_t mask, int64_t nice) : Room("Treasure", std::move(m), mask, nice) {
     switch (randUint() % 4) {
         case 0:
-            description = "You enter a blank room with a small lantern hanging form the ceiling in the center";
+            description = "a blank room with a small lantern hanging form the ceiling in the center";
             break;
         case 1:
-            description = "You enter a peaceful room covered in moss. A small fountain is in the center";
+            description = "a peaceful room covered in moss. A small fountain is in the center";
             break;
         case 2:
-            description = "You enter a room covered in cobwebs. No enemies seem to be near... nor anything alive";
+            description = "a room covered in cobwebs. No enemies seem to be near... nor anything alive";
             break;
         default:
-            description = "You enter a room with a mop. Not much to look at";
+            description = "a room with a mop. Not much to look at";
             break;
     }
     //Switch statement for spawn table
