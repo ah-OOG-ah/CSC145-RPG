5d1331c8f489a2f3c81756ae863a211d7b69e4cf
Joseph Racca <jracca001@student.franciscan.edu>
2024-05-01 11:58:13 -0400

    Made some new dialogue to wise tree

--- scene/rooms/TreasureRoom.cpp
+++ scene/rooms/TreasureRoom.cpp
@@ -10,8 +10,11 @@ TreasureRoom::TreasureRoom(std::shared_ptr<Movement> m, uint8_t mask, int64_t ni
             description = "an earthy room with a large tree filling the center";
             dialogueHead = std::make_shared<MenuNode>("Welcome to my grove.");
             auto hostile = dialogueHead->addChild("Strike the tree", dialogueHead);
-            dialogueHead->addChild("Hug the tree", dialogueHead)->flag = 1;
-            hostile->addChild("You dare savage my bark!?!", hostile)->flag = -1;
+            auto friendly = dialogueHead->addChild("Hug the tree", dialogueHead)->flag = 1;
+            hostile = hostile->addChild("You dare savage my bark!?!", hostile);
+            hostile->addChild("Say sorry", hostile)->flag = 0;
+            hostile->addChild("Strike again", hostile)->flag = -1;
+            hostile->addChild("Insult mother", hostile)->flag = -2;
             break;
     }
 }
@@ -24,15 +27,20 @@ void TreasureRoom::specialAction() {
     }
 
     /**
+     * -2 - Always battle
      * -1 - 50/50 hostile v die
      *  0 - nothing
      *  1 - 50/50 nothing v treasure
      */
     switch (node->flag) {
+        case -2:
+            std::cout << entityName << " defends his mother\'s honor with a most hateful rage!" << std::endl;
+            Battle(entityName, 10).run();
+            break;
         case -1:
             if (randBool()) {
                 std::cout << entityName << " won't go down without a fight!" << std::endl;
-                Battle(entityName, 20).run();
+                Battle(entityName, 7).run();
             } else {
                 std::cout << "Despite its posturing, " << entityName << " dies in just a few swings." << std::endl;
             }
